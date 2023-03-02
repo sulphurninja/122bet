ca28bf5418bf01755002f38925ed7698f371accc
sulphurninja <sulphurninja@gmail.com>
2023-03-02 07:54:16 +0530

    work

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = nextConfig
 
 module.exports={
   env:{
-    "BASE_URL": "http://localhost:3000",
+    "BASE_URL": "http://bet12.vercel.app",
     "MONGODB_URI":"mongodb+srv://aditya4sure:RiseAbove@10bet.tld0wb0.mongodb.net/?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET":"2MqS&V^f_&UudgstXMbnd3LTdC_zkSzBYVB#Q=9%Rjm&fFBW6zTE3^s646kJ",
     "REFRESH_TOKEN_SECRET":"S%=VY!nj3rkK@?VZ_v?bQVFzhz^rSA#vKgJ4Rb23j_XCjQXU?fY9AmA-MgJWHae9f*Xz%P*x$WaGT*7c8HM67B-R%Z_zcy"
