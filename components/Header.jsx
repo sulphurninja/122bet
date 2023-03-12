55231523eebc48ecf4ba5b8ef74c2520cb9fa35e
sulphurninja <sulphurninja@gmail.com>
2023-03-12 13:57:18 +0530

    table

--- components/Header.jsx
+++ components/Header.jsx
@@ -91,11 +91,13 @@ function Header() {
 
   useEffect(() => {
     if (Object.keys(auth).length > 0) {
+      if (userName) {
       axios.get(`/api/user/balance?userName=${userName}`)
         .then(response => {
           setBalance(response.data.balance)
         })
         .catch(error => console.error(error))
+      }
     }
   }, [auth])
 
