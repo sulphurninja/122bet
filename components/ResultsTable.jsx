73ead8cfdcd19ed12fe7bda8e5aefd1f2a160f8d
sulphurninja <sulphurninja@gmail.com>
2023-03-12 13:51:31 +0530

    table

--- components/ResultsTable.jsx
+++ components/ResultsTable.jsx
@@ -1,108 +1,86 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ResultsTable() {
   const [results, setResults] = useState([]);
   const [drawTimes, setDrawTimes] = useState([]);
-  const [nextToDrawtime, setNextToDrawtime] = useState('');
-  const [timeToDraw, setTimeToDraw] = useState('');
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const now = new Date();
-      const nextToDraw = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        now.getHours(),
-        now.getMinutes() + 1,
-        0,
-        0
-      );
-      const timeDiff = Math.floor((nextToDraw - now) / 1000);
-      const seconds = timeDiff % 60;
-      const newTimeToDraw = `${seconds.toString().padStart(2, "0")}`;
-      const newNextToDrawtime = nextToDraw.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-      setNextToDrawtime(newNextToDrawtime);
-      setTimeToDraw(newTimeToDraw);
-    }, 1000);
-
+  const calculateDrawTimes = () => {
     const startDrawTime = new Date();
     startDrawTime.setMinutes(Math.floor(startDrawTime.getMinutes() / 5) * 5);
     startDrawTime.setSeconds(0);
-    const drawTimes = Array(10).fill().map((_, index) => {
-      const drawTime = new Date(startDrawTime.getTime() - index * 5 * 60 * 1000);
-      return drawTime.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-    });
-
-    setDrawTimes(drawTimes);
-
-    const fetchResults = async (drawTime) => {
-      const response = await fetch(`/api/getPrior?drawTime=${drawTime}`);
-      const result = await response.json();
-      return result.winningNumber;
-    }
-  
-    const checkForNewResults = () => {
-      const now = new Date();
-      const currentDrawTime = drawTimes.find(drawTime => {
-        const [hours, minutes] = drawTime.split(':');
-        const drawTimeDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
-        return drawTimeDate <= now;
-      });
-  
-      if (currentDrawTime) {
-        fetchResults(currentDrawTime).then(newResult => {
-          setResults(prevResults => [...prevResults.slice(1), newResult]);
+    const drawTimes = Array(10)
+      .fill()
+      .map((_, index) => {
+        const drawTime = new Date(
+          startDrawTime.getTime() - index * 5 * 60 * 1000
+        );
+        return drawTime.toLocaleString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
         });
-      }
-    };
-  
-    checkForNewResults();
-  
-    const intervalId = setInterval(() => {
-      checkForNewResults();
-    }, 1000);
-  
-    return () => {
-      clearInterval(intervalId);
+      });
+    setDrawTimes(drawTimes);
+  };
+
+  const getPriorCopy = async (drawTime) => {
+    try {
+      const res = await axios.get(`/api/getPriorcopy?drawTime=${drawTime}`);
+      return res.data.winningNumber;
+    } catch (error) {
+      console.log(error);
     }
+  };
+  useEffect(() => {
+    calculateDrawTimes();
   }, []);
+
+
+  const fetchResults = async () => {
+    const results = await Promise.all(
+      drawTimes.map((drawTime) => getPriorCopy(drawTime))
+    );
+    setResults(results);
+  };
+
+  useEffect(() => {
+      fetchResults();
+  }, [drawTimes]);
 
   const getRowClassName = (winningNumber) => {
     if (winningNumber === 1 || winningNumber === 5 || winningNumber === 9) {
-      return 'bg-red-500';
+      return "bg-red-500";
     }
     if (winningNumber === 2 || winningNumber === 6 || winningNumber === 10) {
-      return 'bg-yellow-500';
-          
+      return "bg-yellow-500";
     }
     if (winningNumber === 3 || winningNumber === 7 || winningNumber === 11) {
-      return 'bg-green-500';
+      return "bg-green-500";
     }
-    if (winningNumber === 4  || winningNumber === 8 || winningNumber === 12) {
-      return 'bg-blue-500';
+    if (winningNumber === 4 || winningNumber === 8 || winningNumber === 12) {
+      return "bg-blue-500";
     }
-    return '';
   };
-
+console.log(results)
   return (
-    <div className=' '>
-    <table className='text-white w-[300px] font-bold  text-center '>
-      <thead>
-        <tr className=''>
-          <th>Time</th>
-          <th>Result</th>
-        </tr>
-      </thead>
-      <tbody className='text-center'>
-        {drawTimes.map((drawTime, index) => (
-          <tr key={drawTime} className={getRowClassName(results[index])}>
-            <td className='border-2'>{drawTime}</td>
-            <td className='border-2'>{index < results.length ? results[index] : '-'}</td>
+    <div className=" ">
+      <table className="text-white h-[400px] w-[400px] font-bold  text-center ">
+        <thead>
+          <tr className="">
+            <th>Time</th>
+            <th>Result</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-center">
+          {drawTimes.map((drawTime, index) => (
+            <tr key={drawTime} className={getRowClassName(results[index])}>
+              <td className="border-2">{drawTime}</td>
+              <td className="border-2">{results[index] || "-"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- pages/api/getPrior.js
+++ pages/api/getPrior.js
@@ -15,34 +15,14 @@ export default async function handler(req, res) {
     // Extract the draw time from the request payload
     const { drawTime } = req.query;
 
-    // Get the current time
-    const now = new Date();
-
-    // If the current time is after the specified draw time, return an error
-    const nextToDraw = new Date(
-      now.getFullYear(),
-      now.getMonth(),
-      now.getDate(),
-      now.getHours(),
-      now.getMinutes() + 1,
-      0,
-      0
-    );
-    if (nextToDraw > new Date(drawTime)) {
-      return res.status(400).json({ message: 'Draw time has already passed' });
+    // Find the document with the corresponding draw time
+    const result = await collection.findOne({ drawTime: { $eq: drawTime } });
+    // If a document is found, return the winning number
+    if (result) {
+      res.status(200).json({ winningNumber: result.winningNumber });
+    } else {
+      res.status(404).json({ message: `No winning number found for draw time: ${drawTime}` });
     }
-
-    // Find the 10 most recent documents with the corresponding draw time
-    const results = await collection.find({ drawTime: { $eq: drawTime } }).sort({ _id: -1 }).limit(10).toArray();
-
-    // If no documents are found, return an error
-    if (results.length === 0) {
-      return res.status(404).json({ message: `No winning numbers found for draw time: ${drawTime}` });
-    }
-
-    // Return the winning numbers
-    const winningNumbers = results.map(result => result.winningNumber);
-    res.status(200).json({ winningNumbers });
 
   } catch (err) {
     console.log('Error connecting to database:', err);
